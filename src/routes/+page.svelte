92202ad014dcd3f3cbf20c9a7ea751152e86a9af
Alex <cawasp@gmail.com>
2025-05-09 23:49:31 -0700

    updated to work with RV plans pdfs + improved styling + search opens pdf on right page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,12 +19,30 @@
                 },
             });
             server = await response.json();
-            for (let i = 0; i < server.length - 1; i++) {
+            /*for (let i = 0; i < server.length - 1; i++) {
                 let path = "";
                 img_paths.push("");
             }
             console.log(server);
-            test = JSON.stringify(server);
+            /*for (let i = 0; i < server["found"].length; i++) {
+                let delta = 0;
+                let match = [0,0]
+                let indices = server["found"][i]["matches"][0]["indices"];
+
+                for (let ind = 0; ind+1 < indices.length; ind++) {
+                    if (ind % 2 == 0) {
+                        if (ind+1 - ind > delta) {
+                            delta = ind+1 - ind;
+                            match[0] = ind;
+                            match[1] = ind+1;
+                        }
+                    }
+                }
+
+                server["found"][i]["match"] = server["found"][i]["item"]["content"].slice(match[0], match[1]+1)
+                console.log("matches: "+server["found"][i]["matches"][0]["indices"][4]);
+                console.log("matches: "+server["found"][i]["matches"][0]["indices"][5]);
+            }*/
         }}
     />
     <!-- <p>{test} {test}</p> -->
@@ -32,12 +50,15 @@
     {#each server["found"] as item}
         <!-- {JSON.stringify(item["item"])} -->
         <a
-            class="self-center flex justify-between flex-row w-150 h-[300px] border-solid border-2 border-black hover:border-orange-600 p-2 mb-[20px]"
-            href="pdf_plans/DJI_manual_{item['item']['page']}.pdf"
+            class="group self-center flex justify-between flex-col w-[600px] h-[450px] border-solid border-2 border-black hover:border-orange-600 p-2 mb-[20px]"
+            href="pdf_plans/{item['item']['section']}.pdf#page={item['item']['page']}"
+            target="_blank"
         >
-            <img src="img_plans/{item['item']['img']}" class="h-[280px]" />
-            <div class="self-end text-3xl">{item["item"]["page"]}</div>
+            <h1 class="self-center text-3xl font-bold group-hover:text-orange-600">Section {item['item']['section']}</h1>
+            <img src="img_plans/{item['item']['section']}/{item['item']['section']}_{item['item']['page']}-1.jpg" class="self-center h-[280px] w-[433px]" />
+            <!--<p>{item["match"]}</p>-->
+            <div class="self-end text-3xl font-bold group-hover:text-orange-600">{item["item"]["page"]}</div>
         </a>
-        <!-- <SearchResult page={item["item"]["page"]} /> -->
+        <!-- <SearchResult page={item["item"]["page"]} />w-[600px] h-[450px] -->
     {/each}
 </div>
--- src/routes/search/+server.js
+++ src/routes/search/+server.js
@@ -1,18 +1,20 @@
 import { json } from "@sveltejs/kit";
 import Fuse from "fuse.js";
-import data from "../../../static/dji_man.json";
+import data from "../../../static/indexed_plans.json";
 
 const fuseOptions = {
   keys: ["content"],
+  includeMatches: "true"
 };
 
 export async function POST({ request, cookies }) {
   const client = await request.json();
 
-  console.log(client);
-  console.log(data[0]);
+  //console.log(client);
+  //console.log(data[0]);
   const fuse = new Fuse(data, fuseOptions);
   const result = fuse.search(client["search"]);
+  //console.log(result)
 
   return json({ found: result });
 }
